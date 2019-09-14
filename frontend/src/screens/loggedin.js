50a996a94228a1a82ccb5919710e09740008f60a
David Li <david.li3100@gmail.com>
2019-09-14 02:28:46 -0400

    basic layout implemented

--- frontend/src/screens/loggedin.js
+++ frontend/src/screens/loggedin.js
@@ -6,6 +6,7 @@ import Appointments from '../Views/Appointments'
 import Patients from '../Views/Patients'
 import firebase from 'firebase';
 import Header from '../components/Header/Header'
+import { Pane } from 'evergreen-ui'
 
 
 export default class LoggedIn extends Component {
@@ -26,21 +27,28 @@ export default class LoggedIn extends Component {
             <Router>
                 <Space.ViewPort>
                     <Space.Left size="15%" scrollable={true}>
-                        <Space.Top size="15%">
-                            Pintox
-                        </Space.Top>
+                        <div className="nav">
+                            <Space.Top size="10%">
+                                <Pane display="flex" height="100%" alignItems="center" justifyContent="center">
+                                    <div className="brand">Pintox</div>
+                                </Pane>
+                            </Space.Top>
+                            <Space.Fill>
+                                Nav
+                            </Space.Fill>
+                        </div>
                     </Space.Left>
                     <Space.Fill>
                         <Space.Top size="10%">
                             <Header user={this.props.user} handleLogout={this.handleLogOut}/>
                         </Space.Top>
                         <Space.Fill scrollable={true}>
-                        <Fragment>
-                            <Route exact path="/" component={Dashboard}/>
-                            <Route path="/dashboard" component={Dashboard}/>
-                            <Route path="/appointments" component={Appointments}/>
-                            <Route path="/patients" component={Patients}/>
-                        </Fragment>                    
+                            <Fragment>
+                                <Route exact path="/" component={Dashboard}/>
+                                <Route path="/dashboard" component={Dashboard}/>
+                                <Route path="/appointments" component={Appointments}/>
+                                <Route path="/patients" component={Patients}/>
+                            </Fragment>                    
                         </Space.Fill>
                     </Space.Fill>
                 </Space.ViewPort>
